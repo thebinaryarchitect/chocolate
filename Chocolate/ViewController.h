ba9ac2033310510fd0b41bf2387ee3f4f079334a
Xiao Yao <xiaoyao@Xiaos-MacBook-Pro.local>
2015-11-11 17:44:44 -0500

    Initial Commit

---
+++ Chocolate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chocolate
+//
+//  Created by Xiao Yao on 11/11/15.
+//  Copyright (c) 2015 Xiao Yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
