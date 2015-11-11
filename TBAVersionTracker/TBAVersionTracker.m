e79fe313ebabceb66bb81d8a52753af334ef4508
Xiao Yao <xiaoyao@Xiaos-MacBook-Pro.local>
2015-11-11 18:06:00 -0500

    Add tracking

--- TBAVersionTracker/TBAVersionTracker.m
+++ TBAVersionTracker/TBAVersionTracker.m
@@ -33,9 +33,16 @@ NSString *const NSUserDefaultsKeyVersionDictionary = @"com.thebinaryarchitect.ve
 #pragma mark - TBAVersionTracker
 
 @interface TBAVersionTracker()
+
 @property (nonatomic, assign, readwrite) BOOL isFirstLaunchEver;
 @property (nonatomic, assign, readwrite) BOOL isFirstLaunchForCurrentVersion;
 @property (nonatomic, assign, readwrite) BOOL isFirstLaunchForCurrentBuild;
+
+@property (nonatomic, strong, readwrite) NSMutableDictionary *versionsDictionary;
+
+@property (nonatomic, strong, readwrite) NSString *currentVersionString;
+@property (nonatomic, strong, readwrite) NSString *currentBuildString;
+
 @end
 
 @implementation TBAVersionTracker
@@ -49,10 +56,63 @@ NSString *const NSUserDefaultsKeyVersionDictionary = @"com.thebinaryarchitect.ve
     return _versionTracker;
 }
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.currentVersionString = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+        self.currentBuildString = [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString *)kCFBundleVersionKey];
+    }
+    return self;
+}
+
 #pragma mark Public
 
 - (void)track {
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSDictionary *dict = [userDefaults versionDictionary];
     
+    BOOL save = NO;
+    
+    // If the version dict does not exist, then it is the first time the user has launched the application after installation.
+    if (dict) {
+        self.isFirstLaunchEver = NO;
+        
+        self.versionsDictionary = [NSMutableDictionary dictionaryWithDictionary:dict];
+        
+        NSArray *versionBuilds = self.versionsDictionary[self.currentVersionString];
+        // If the versionBuilds array does not exist, then it is the first time the user has launched the the current version of the application.
+        if (versionBuilds) {
+            self.isFirstLaunchForCurrentVersion = NO;
+            // If the build string does not exist, then it is the first time the user has launched the the current build of the application.
+            if ([versionBuilds containsObject:self.currentBuildString]) {
+                self.isFirstLaunchForCurrentBuild = NO;
+            } else {
+                self.isFirstLaunchForCurrentBuild = YES;
+                NSMutableArray *updatedVersionBuilds = [NSMutableArray arrayWithArray:versionBuilds];
+                [updatedVersionBuilds addObject:self.currentBuildString];
+                [self.versionsDictionary setObject:updatedVersionBuilds forKey:self.currentVersionString];
+                save = YES;
+            }
+        } else {
+            self.isFirstLaunchForCurrentVersion = YES;
+            self.isFirstLaunchForCurrentBuild = YES;
+            versionBuilds = @[self.currentBuildString];
+            [self.versionsDictionary setObject:versionBuilds forKey:self.currentVersionString];
+            save = YES;
+        }
+    } else {
+        self.isFirstLaunchEver = YES;
+        self.isFirstLaunchForCurrentVersion = YES;
+        self.isFirstLaunchForCurrentBuild = YES;
+        self.versionsDictionary = [[NSMutableDictionary alloc] init];
+        NSArray *builds = @[self.currentBuildString];
+        [self.versionsDictionary setObject:builds forKey:self.currentVersionString];
+        save = YES;
+    }
+    
+    if (save) {
+        [userDefaults setVersionDictionary:self.versionsDictionary];
+    }
 }
 
 @end
